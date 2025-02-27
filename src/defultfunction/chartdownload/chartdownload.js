76cb20f33a6a12c7666a058415025f858d7f5c3e
jaysourceid <vinaysakariya5042@gmail.com>
2025-02-27 12:09:16 +0530

    add

--- src/defultfunction/chartdownload/chartdownload.js
+++ src/defultfunction/chartdownload/chartdownload.js
@@ -183,7 +183,7 @@ export const handleDownload = (pickupsit, chartData) => {
               <div class="font-bold"><span class="text-red-800">ડ્રાઈવર:</span> ${
                 chartData.driver
               }</div>
-              <div class="font-bold"><span class="text-red-800">મોબાઇલ નંબર:</span> ${
+              <div class="font-bold"><span class="text-red-800">મો.નંબર:</span> ${
                 chartData.driver
               }</div>
               <div class="font-bold"><span class="text-red-800">તારીખ:</span> ${formatDate(
